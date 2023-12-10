0cf7df9bbb0b668d52348261602e82620bb94c5b
MNunez <manu.nunez.2004@gmail.com>
2023-12-09 22:19:55 -0600

    js -> json

--- sources/scripts/new_client.js
+++ sources/scripts/new_client.js
@@ -1,9 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
     var agregarBtn = document.querySelector("#agregarBtn");
     var agregarModal = new bootstrap.Modal(document.getElementById("agregarModal"));
-  
+
     agregarBtn.addEventListener("click", function () {
-      agregarModal.show();
+        agregarModal.show();
     });
-  });
-  
+
+    var tipoPagoSelect = document.getElementById("tipoPago");
+    var pagos = document.querySelectorAll(".pago");
+
+    tipoPagoSelect.addEventListener("change", function () {
+        pagos.forEach(function (pago) {
+            pago.style.display = "none";
+        });
+
+        var selectedOption = tipoPagoSelect.value;
+
+        if (selectedOption === "anual") {
+            document.getElementById("pago1").style.display = "block";
+        } else if (selectedOption === "semestral") {
+            document.getElementById("pago1").style.display = "block";
+            document.getElementById("pago2").style.display = "block";
+        } else if (selectedOption === "trimestral") {
+            document.getElementById("pago1").style.display = "block";
+            document.getElementById("pago2").style.display = "block";
+            document.getElementById("pago3").style.display = "block";
+            document.getElementById("pago4").style.display = "block";
+        } else {
+            
+        }
+    });
+});
